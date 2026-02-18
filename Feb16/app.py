20f86d1598af31a80461d65e5da6f878f56d1a8b
jamesgotch <jimmygotch@icloud.com>
2026-02-18 15:49:23 +0000

    Feb18 commit

--- Feb16/app.py
+++ Feb16/app.py
@@ -6,15 +6,15 @@ import gradio as gr
 conn = sqlite3.connect('../baseball.db')
 cursor = conn.cursor()
 query = """
-    WITH top_hitters AS (SELECT people.nameFirst, people.nameLast
+    WITH top_hitters AS (SELECT nameFirst, nameLast, batting.playerID
     FROM batting INNER JOIN people
     ON batting.playerID = people.playerID
     WHERE teamID = 'PHI'
     GROUP BY batting.playerID
     ORDER BY sum(HR) DESC
-    LIMIT 10)
+    LIMIT 20)
 
-    SELECT CONCAT(nameFirst, ' ', nameLast) AS player
+    SELECT CONCAT(nameFirst, ' ', nameLast) AS player, playerID
     FROM top_hitters
     ORDER BY nameLast ASC
 """
@@ -24,16 +24,27 @@ records = cursor.fetchall()
 conn.close()
 
 
-players = []
-for record in records:
-    players.append(record[0])
+# players = []
+# for record in records:
+#     players.append(record[0])
 
-def get_player_data(player):
-    return f"Data for {player}"
+def get_data(player):
+    conn = sqlite3.connect('../baseball.db')
+    cursor = conn.cursor()
+    query = """
+        SELECT yearID, HR
+        FROM batting
+        WHERE playerID = ?
+    """
+    cursor.execute(query, (player,))
+    data = cursor.fetchall()
+    conn.close()
+    df = pd.DataFrame(data, columns=['yearID', 'HR'])
+    return df
 
-iface = gr.Interface(
-    fn = get_player_data,
-    inputs = gr.Dropdown(choices = players, label = "Select a Player"),
-    outputs = gr.Textbox(label = "Player Data")
-)
+with gr.Blocks() as iface:
+    dropdown = gr.Dropdown([(r[0], r[1]) for r in records], interactive=True)
+    plot = gr.LinePlot(x='yearID', y='HR')
+    dropdown.change(get_data, inputs=dropdown, outputs=plot)
+
 iface.launch()
